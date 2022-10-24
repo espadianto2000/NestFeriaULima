ab27fd2ff1679b3907bbbbf52a0f4c5906c7e33f
MatiasV-26 <matias.villanueva.paz@gmail.com>
2022-10-24 18:10:31 -0500

    ModelVisita

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { getDatabase, ref, set } from 'firebase/database';
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ModelPrueba } from "./models/modelPrueba";
+import { modelVisita } from "./models/modelVisita";
 
 @Controller()
 export class AppController {
@@ -29,4 +30,23 @@ export class AppController {
     var prueba :ModelPrueba = body;
     set(ref(database, 'prueba/'+prueba.categoria+'/'+prueba.numero+'/pruebaInterna'), prueba);
   }
+
+
+  @Post('/Visita')
+  PruebaVisita(@Body() body:modelVisita): void {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBg7eR4Xev2MVxEMWeRczvYBBG-q2oSbas",
+      authDomain: "nestferia.firebaseapp.com",
+      databaseURL: "https://nestferia-default-rtdb.firebaseio.com",
+      projectId: "nestferia",
+      storageBucket: "nestferia.appspot.com",
+      messagingSenderId: "981551225191",
+      appId: "1:981551225191:web:3b49b4fffa5b53128d37c2"
+    };
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    var Visita :modelVisita = body;
+    set(ref(database, 'Visita/'+Visita.empresaV+'/'+Visita.fecha+'/'+Visita.idV), Visita);
+  }
+  
 }
---
+++ src/models/modelVisita.ts
@@ -0,0 +1,7 @@
+export class modelVisita{
+    carreraV: string;
+    empresaV: string;
+    idV: string;
+    fecha: string;
+    estadoV: string;
+}
