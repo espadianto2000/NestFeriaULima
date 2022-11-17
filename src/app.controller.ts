fc9a85b41e7191205c3692caa4c71e7db6b8c4d6
espadianto2000 <20170532@aloe.ulima.edu.pe>
2022-11-17 13:46:02 -0500

    fix multiples instancias de database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,7 +11,21 @@ import { modelEntradaStand } from "./models/modelEntradaStand";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  database:any;
+  constructor(private readonly appService: AppService) {
+    console.log("prueba 1")
+    const firebaseConfig = {
+      apiKey: "AIzaSyBg7eR4Xev2MVxEMWeRczvYBBG-q2oSbas",
+      authDomain: "nestferia.firebaseapp.com",
+      databaseURL: "https://nestferia-default-rtdb.firebaseio.com",
+      projectId: "nestferia",
+      storageBucket: "nestferia.appspot.com",
+      messagingSenderId: "981551225191",
+      appId: "1:981551225191:web:3b49b4fffa5b53128d37c2"
+    };
+    const app = initializeApp(firebaseConfig);
+    this.database = getDatabase(app);
+  }
 
   @Get()
   getHello(): string {
@@ -19,87 +33,33 @@ export class AppController {
   }
   @Post('/prueba')
   muerteAdapt(@Body() body:ModelPrueba): void {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBg7eR4Xev2MVxEMWeRczvYBBG-q2oSbas",
-      authDomain: "nestferia.firebaseapp.com",
-      databaseURL: "https://nestferia-default-rtdb.firebaseio.com",
-      projectId: "nestferia",
-      storageBucket: "nestferia.appspot.com",
-      messagingSenderId: "981551225191",
-      appId: "1:981551225191:web:3b49b4fffa5b53128d37c2"
-    };
-    const app = initializeApp(firebaseConfig);
-    const database = getDatabase(app);
     var prueba :ModelPrueba = body;
-    set(ref(database, 'prueba/'+prueba.categoria+'/'+prueba.numero+'/pruebaInterna'), prueba);
+    set(ref(this.database, 'prueba/'+prueba.categoria+'/'+prueba.numero+'/pruebaInterna'), prueba);
   }
 
 
   @Post('/Visita')
   PruebaVisita(@Body() body:modelVisita): void {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBg7eR4Xev2MVxEMWeRczvYBBG-q2oSbas",
-      authDomain: "nestferia.firebaseapp.com",
-      databaseURL: "https://nestferia-default-rtdb.firebaseio.com",
-      projectId: "nestferia",
-      storageBucket: "nestferia.appspot.com",
-      messagingSenderId: "981551225191",
-      appId: "1:981551225191:web:3b49b4fffa5b53128d37c2"
-    };
-    const app = initializeApp(firebaseConfig);
-    const database = getDatabase(app);
     var Visita :modelVisita = body;
-    set(ref(database, 'Visita/'+Visita.empresaV+'/'+Visita.fecha+'/'+Visita.idV), Visita);
+    set(ref(this.database, 'Visita/'+Visita.empresaV+'/'+Visita.fecha+'/'+Visita.idV), Visita);
   }
 
   @Post('/EntradaZoom')
   PruebaEntradaZoom(@Body() body:modelEntradaZoom): void {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBg7eR4Xev2MVxEMWeRczvYBBG-q2oSbas",
-      authDomain: "nestferia.firebaseapp.com",
-      databaseURL: "https://nestferia-default-rtdb.firebaseio.com",
-      projectId: "nestferia",
-      storageBucket: "nestferia.appspot.com",
-      messagingSenderId: "981551225191",
-      appId: "1:981551225191:web:3b49b4fffa5b53128d37c2"
-    };
-    const app = initializeApp(firebaseConfig);
-    const database = getDatabase(app);
     var entrada :modelEntradaZoom = body;
-    set(ref(database, 'EntradaZoom/'+entrada.empresaV+'/'+entrada.fecha+'/'+entrada.idV), entrada);
+    set(ref(this.database, 'EntradaZoom/'+entrada.empresaV+'/'+entrada.fecha+'/'+entrada.idV), entrada);
   }
 
   @Post('/Ingresos')
   PruebaIngresos(@Body() body:modelIngresos): void {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBg7eR4Xev2MVxEMWeRczvYBBG-q2oSbas",
-      authDomain: "nestferia.firebaseapp.com",
-      databaseURL: "https://nestferia-default-rtdb.firebaseio.com",
-      projectId: "nestferia",
-      storageBucket: "nestferia.appspot.com",
-      messagingSenderId: "981551225191",
-      appId: "1:981551225191:web:3b49b4fffa5b53128d37c2"
-    };
-    const app = initializeApp(firebaseConfig);
-    const database = getDatabase(app);
+    
     var ingresos :modelIngresos = body;
-    set(ref(database, 'Ingresos/'+ingresos.idV), ingresos);
+    set(ref(this.database, 'Ingresos/'+ingresos.idV), ingresos);
   }
   @Post('/EntradaStand')
   PruebaEntradaStand(@Body() body:modelEntradaStand): void {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBg7eR4Xev2MVxEMWeRczvYBBG-q2oSbas",
-      authDomain: "nestferia.firebaseapp.com",
-      databaseURL: "https://nestferia-default-rtdb.firebaseio.com/",
-      projectId: "nestferia",
-      storageBucket: "nestferia.appspot.com",
-      messagingSenderId: "981551225191",
-      appId: "1:981551225191:web:3b49b4fffa5b53128d37c2"
-    };
-    const app = initializeApp(firebaseConfig);
-    const database = getDatabase(app);
     var entradaS :modelEntradaStand = body;
-    set(ref(database, 'EntradaStand/'+entradaS.empresaV+'/'+entradaS.fecha+'/'+entradaS.idV), entradaS);
+    set(ref(this.database, 'EntradaStand/'+entradaS.empresaV+'/'+entradaS.fecha+'/'+entradaS.idV), entradaS);
     
   }
 }
